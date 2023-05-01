ce550b4ab085c44d05ec58df778389d00dd1b2b7
omera <omeravrech@gmail.com>
2023-05-01 16:35:50 +0300

    add documentations

--- TimeBasedACL/routerfactory.py
+++ TimeBasedACL/routerfactory.py
@@ -4,15 +4,19 @@ from enums import Vendors
 from ping3 import ping
 from socket import gethostbyname
 
+
 class RouterFactory(IFactory):
     @classmethod
-    def generate(cls, vendor:str, address:str, *args, **kwargs) -> IRouter:
-        vendor = getattr(Vendors,vendor.capitalize(),None)
-        
+    def generate(cls, vendor: str, address: str, *args, **kwargs) -> IRouter:
+        """ This class responsible for generating new device
+        """
+        vendor = getattr(Vendors, vendor.capitalize(), None)
+
+        # Verify host is available
         if (address is None) or (ping(address, size=64, timeout=2, seq=1) == 0):
             return None
-        
+
         kwargs["address"] = gethostbyname(address)
-        
+
         if vendor == Vendors.Cisco:
             return CiscoRouter(**kwargs)
